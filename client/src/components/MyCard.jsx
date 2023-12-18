ea2e986077432f838a0d4a513d349a508c1f2732
Dilmand On Mac <dilmand@outlook.de>
2023-12-18 09:31:44 +0100

    last commmit bevor new contract

--- client/src/components/MyCard.jsx
+++ client/src/components/MyCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { tagType, thirdweb } from '../assets';
+import AddReactionSharpIcon from '@mui/icons-material/AddReactionSharp';
 import { daysLeft } from '../utils';
 
 const FundCard = ({ owner, title, description, target, deadline, amountCollected, image, handleClick }) => {
@@ -11,14 +12,9 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
       <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]"/>
 
       <div className="flex flex-col p-4">
-        <div className="flex flex-row items-center mb-[18px]">
-          <img src={tagType} alt="tag" className="w-[17px] h-[17px] object-contain"/>
-          <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">Education</p>
-        </div>
-
         <div className="block">
-          <h3 className="font-epilogue font-semibold text-[16px] text-white text-left leading-[26px] truncate">{title}</h3>
-          <p className="mt-[5px] font-epilogue font-normal text-[#808191] text-left leading-[18px] truncate">{description}</p>
+          <h2 className="font-epilogue font-semibold text-[16px] text-black text-left leading-[26px] truncate">{title}</h2>
+          <p className="mt-[5px] font-epilogue font-normal text-black-900 text-left leading-[18px] truncate">{description}</p>
         </div>
 
         <div className="flex justify-between flex-wrap mt-[15px] gap-2">
@@ -34,7 +30,8 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
 
         <div className="flex items-center mt-[20px] gap-[12px]">
           <div className="w-[30px] h-[30px] rounded-full flex justify-center items-center bg-[#13131a]">
-            <img src={thirdweb} alt="user" className="w-1/2 h-1/2 object-contain"/>
+            
+            <AddReactionSharpIcon alt="user" className="w-1/2 h-1/2 object-contain text-white" />
           </div>
           <p className="flex-1 font-epilogue font-normal text-[12px] text-[#808191] truncate">by <span className="text-[#b2b3bd]">{owner}</span></p>
         </div>
--- client/src/pages/CampaignDetailsPage.jsx
+++ client/src/pages/CampaignDetailsPage.jsx
@@ -1,34 +1,46 @@
-
-import React, { useContext, useState } from 'react';
-import { useParams, useLocation } from 'react-router-dom';
-import { ethers } from 'ethers';
-import { ContractContext } from '../context/ContractContext';
-import {convertToDate} from '../utils';
+import React, { useContext, useState } from "react";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
+import { ContractContext } from "../context/ContractContext";
+import { convertToDate } from "../utils";
 
 const stateDetailsPage = () => {
-  const { id } = useParams(); // Annahme, dass die Kampagnen-ID in der URL verwendet wird
-  const { donateToCampaign } = useContext(ContractContext);
-  const {state} = useLocation();
-  const [donationAmount, setDonationAmount] = useState('');
+  const { donateToCampaign, contract } = useContext(ContractContext);
+  const { state } = useLocation();
+  const [donationAmount, setDonationAmount] = useState("");
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
-
 
   // Funktion, um die Kampagnendetails zu laden (hier pseudo-codeartig angedeutet)
 
   const handleDonate = async () => {
     setIsLoading(true);
-    await donateToCampaign(state.id, donationAmount);
+    if (!donationAmount) {
+      console.log("Bitte geben Sie einen Betrag ein.");
+      return;
+    }
+    const amountInWei = ethers.utils.parseUnits(donationAmount, 'ether').toHexString();;
+    console.log(amountInWei);
+    await donateToCampaign(state.id, amountInWei);
     setIsLoading(false);
+    navigate('/');
   };
+
 
   return (
     <div className="flex flex-col items-center p-4">
       <h1 className="text-2xl font-bold">{state.title}</h1>
-      <img src={state.image} alt={state.title} className="max-w-lg" />
+      <img src={state.image} alt={state.title} style={{ height: '300px', width: '300px', objectFit:'contain' }} />
       <p className="my-4">{state.description}</p>
-      <p><strong>Ziel:</strong> {state.target} ETH</p>
-      <p><strong>Gesammelt:</strong> {state.amountCollected} ETH</p>
-      <p><strong>Deadline:</strong> {convertToDate(state.deadline)}</p>
+      <p>
+        <strong>Ziel:</strong> {state.target} ETH
+      </p>
+      <p>
+        <strong>Gesammelt:</strong> {state.amountCollected} ETH
+      </p>
+      <p>
+        <strong>Deadline:</strong> {convertToDate(state.deadline)}
+      </p>
 
       <div className="my-4">
         <input
@@ -38,11 +50,41 @@ const stateDetailsPage = () => {
           placeholder="Betrag in ETH"
           className="border p-2"
         />
-        <button onClick={handleDonate} className="bg-blue-500 text-white p-2 ml-2">
-          Spenden
+        <button
+          onClick={handleDonate}
+          className="bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          disabled={isLoading} // Deaktiviert die Schaltfläche, während geladen wird
+        >
+          {isLoading ? (
+            <div className="flex items-center justify-center">
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 0116 0H4z"
+                ></path>
+              </svg>
+              Laden...
+            </div>
+          ) : (
+            "Starten"
+          )}
         </button>
       </div>
-      
+
       {/* Hier könnten Sie eine Liste der Spenden und Spender anzeigen */}
     </div>
   );
